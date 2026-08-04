5816a9b841153e41b0d44cee368574eabfda9472
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-04 17:58:10 +0530

    operator-code

---
+++ C++ PROGRAM/Operators/addition_operator.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate addition operator (+)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 20;
+    cout << "Sum = " << a + b << endl;
+    return 0;
+}
