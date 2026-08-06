4e78d6ee6868bf53f30f10246c65460ef3e9d0aa
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-06 16:47:10 +0530

    bitwise_and operator

---
+++ C++ PROGRAM/Operators/bitwise_and.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate bitwise AND operator (&)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 6, b = 4; // 6=110, 4=100
+    cout << "Bitwise AND => " << (a & b) << endl;
+    return 0;
+}
