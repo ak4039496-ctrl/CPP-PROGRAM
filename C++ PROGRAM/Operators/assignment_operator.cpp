105b2a618e677728a6e80c0574c75cb1fb6b535b
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-06 16:46:27 +0530

    assignment_operator

---
+++ C++ PROGRAM/Operators/assignment_operator.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate assignment operator (=)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    a = 100; // assignment
+    cout << "Value of a => " << a << endl;
+    return 0;
+}
