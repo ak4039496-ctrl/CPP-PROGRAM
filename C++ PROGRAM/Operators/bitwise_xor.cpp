2eb5891b3dc07be67ab28003e64a3acea6863daa
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-06 16:51:22 +0530

    bitwise_xor

---
+++ C++ PROGRAM/Operators/bitwise_xor.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate bitwise XOR operator (^)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 6, b = 4; // 6=110, 4=100
+    cout << "Bitwise XOR = " << (a ^ b) << endl;
+    return 0;
+}
