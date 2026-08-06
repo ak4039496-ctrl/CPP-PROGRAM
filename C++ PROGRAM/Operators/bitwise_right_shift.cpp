6a44dd7e85d8071a9792c1ffdc398fbd40e1881b
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-06 16:50:17 +0530

    bitwise_right_shift

---
+++ C++ PROGRAM/Operators/bitwise_right_shift.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate bitwise right shift operator (>>)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20; // binary 10100
+    cout << "Right Shift (a>>2) => " << (a >> 2) << endl;
+    return 0;
+}
