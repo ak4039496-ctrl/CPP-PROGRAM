0e4970f5d3408c430c7b6f8a5b528675f4b9c487
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-06 16:48:02 +0530

    bitwise_not_operator

---
+++ C++ PROGRAM/Operators/bitwise_not_operator.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate bitwise NOT operator (~)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5; // binary 0101
+    cout << "Bitwise NOT of a = " << ~a << endl;
+    return 0;
+}
