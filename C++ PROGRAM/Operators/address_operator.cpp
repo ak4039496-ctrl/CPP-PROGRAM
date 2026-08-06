cd78111f1e855d7ba75292608ff270575d779344
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-06 16:45:18 +0530

    address_operator

---
+++ C++ PROGRAM/Operators/address_operator.cpp
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate address-of operator (&)
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 25;
+    cout << "Address of a => " << &a << endl;
+    return 0;
+}
