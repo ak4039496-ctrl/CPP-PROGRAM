ec8e8b66cd80809c49d1785f8dbbdca81165c896
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-06 16:47:34 +0530

    bitwise_left_shift

---
+++ C++ PROGRAM/Operators/bitwise_left_shift.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate bitwise left shift operator (<<)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5; // binary 0101
+    cout << "Left Shift (a<<1) => " << (a << 1) << endl;
+    return 0;
+}
