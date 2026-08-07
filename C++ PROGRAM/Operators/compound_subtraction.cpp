d0751c94da36e732e0fa492b71feb10870d3ea6a
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:09:14 +0530

    compound_subtraction

---
+++ C++ PROGRAM/Operators/compound_subtraction.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate compound assignment operator (-=)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20;
+    a -= 7; // same as a = a - 7
+    cout << "Value of a => " << a << endl;
+    return 0;
+}
