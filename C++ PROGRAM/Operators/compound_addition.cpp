ad42e3287f0950826a9c40a3e56baaa2dcd237f3
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:04:01 +0530

    compound_addition

---
+++ C++ PROGRAM/Operators/compound_addition.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate compound assignment operator (+=)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    a += 5; // same as a = a + 5
+    cout << "Value of a => " << a << endl;
+    return 0;
+}
