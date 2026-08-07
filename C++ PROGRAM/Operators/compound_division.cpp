bf0a25623d9d4159eaf9268659f127b356bdd9bd
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:04:33 +0530

    compound_division

---
+++ C++ PROGRAM/Operators/compound_division.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate compound assignment operator (/=)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20;
+    a /= 4; // same as a = a / 4
+    cout << "Value of a = " << a << endl;
+    return 0;
+}
