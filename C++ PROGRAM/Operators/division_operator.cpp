6ec9a6e917231fa3dbc44782beb190d583de8263
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:11:32 +0530

    division_operator

---
+++ C++ PROGRAM/Operators/division_operator.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate division operator (/)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 40, b = 5;
+    cout << "Quotient => " << a / b << endl;
+    return 0;
+}
