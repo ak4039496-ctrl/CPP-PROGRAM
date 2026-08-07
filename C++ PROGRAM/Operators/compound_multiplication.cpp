3deb4314d4ecebc2d24b2f4826076fcf9c3c6a92
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:08:45 +0530

    compound_multiplication

---
+++ C++ PROGRAM/Operators/compound_multiplication.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate compound assignment operator (*=)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4;
+    a *= 3; // same as a = a * 3
+    cout << "Value of a => " << a << endl;
+    return 0;
+}
