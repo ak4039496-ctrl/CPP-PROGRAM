f01ec75dda353f1d652a60a48ac8b5f438e3aa98
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:12:42 +0530

    increment_operator

---
+++ C++ PROGRAM/Operators/increment_operator.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate increment operator (++)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    cout << "Original a => " << a << endl;
+    a++;
+    cout << "After increment a => " << a << endl;
+    return 0;
+}
