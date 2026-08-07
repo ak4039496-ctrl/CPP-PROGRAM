c0673f7aa44df346935587db76af1b1fe50a51eb
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:10:45 +0530

    decrement_operator

---
+++ C++ PROGRAM/Operators/decrement_operator.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate decrement operator (--)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    cout << "Original a => " << a << endl;
+    a--;
+    cout << "After decrement a => " << a << endl;
+    return 0;
+}
