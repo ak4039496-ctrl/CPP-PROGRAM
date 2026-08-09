45cb4c1a6bde0008ff19fa179c5284090ccc121f
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-09 07:21:26 +0530

    modulus_operator

---
+++ C++ PROGRAM/Operators/modulus_operator.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate modulus operator (%)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 17, b = 5;
+    cout << "Remainder => " << a % b << endl;
+    return 0;
+}
