61938648b969f1171e880b811e0480787ceb69a4
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-09 07:05:24 +0530

    logical_not_operator

---
+++ C++ PROGRAM/Operators/logical_not.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Amit Gupta
+Date: 06-08-2026
+Program: Demonstrate logical NOT operator (!)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool flag = false;
+    if (!flag)
+        cout << "Flag is false" << endl;
+    else
+        cout << "Flag is true" << endl;
+    return 0;
+}
