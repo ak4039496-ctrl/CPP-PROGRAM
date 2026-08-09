dcdd1e9b85b521e18ebaf250896915e68bd650b0
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-09 07:22:06 +0530

    multiplication_operator

---
+++ C++ PROGRAM/Operators/multiplication_operator.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate multiplication operator (*)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7, b = 6;
+    cout << "Product => " << a * b << endl;
+    return 0;
+}
