9b4097f2cc66bed10acda820b206b4a567572cde
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-10 21:03:59 +0530

    relational_not_equal

---
+++ C++ PROGRAM/Operators/relational_not_equal.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate relational operator (!=)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 15;
+    if (a != b)
+        cout << "a and b are not equal" << endl;
+    else
+        cout << "a and b are equal" << endl;
+    return 0;
+}
