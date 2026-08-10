7e6ea52d69dab9454a7a7cad509f8a800c61bb1f
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-10 21:11:24 +0530

    typecast_operator

---
+++ C++ PROGRAM/Operators/typecast_operator.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate typecast operator
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7, b = 2;
+    double result = (double)a / b; // typecasting
+    cout << "Result => " << result << endl;
+    return 0;
+}
