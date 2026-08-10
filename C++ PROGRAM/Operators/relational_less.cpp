488ff339234a9ff126f67cf9939646721fd02447
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-10 21:00:53 +0530

    relational_less

---
+++ C++ PROGRAM/Operators/relational_less.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate relational operator (<)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 15, b = 30;
+    if (a < b)
+        cout << "a is less than b" << endl;
+    else
+        cout << "a is not less than b" << endl;
+    return 0;
+}
