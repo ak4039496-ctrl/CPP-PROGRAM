f02dc082ad76b1550a7246e11aa0283780ac15f8
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-10 21:06:56 +0530

    subtraction_operator

---
+++ C++ PROGRAM/Operators/subtraction_operator.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate subtraction operator (-)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 50, b = 30;
+    cout << "Difference => " << a - b << endl;
+    return 0;
+}
