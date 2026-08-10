ddf2c210d10d38e4e2f464a840ad3a9f6881a9df
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-10 20:59:53 +0530

    relational_greater

---
+++ C++ PROGRAM/Operators/relational_greater.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Amit Gupta
+Date: 04-08-2026
+Program: Demonstrate relational operator (>)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 25, b = 20;
+    if (a > b)
+        cout << "a is greater than b" << endl;
+    else
+        cout << "a is not greater than b" << endl;
+    return 0;
+}
