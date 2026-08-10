4a0af388a1f2cea2307889cef8dedbf3d0d1c9e9
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-10 21:05:59 +0530

    sizeof_operator

---
+++ C++ PROGRAM/Operators/sizeof_operator.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 07-08-2026
+Program: Demonstrate sizeof operator
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Size of int => " << sizeof(int) << " bytes" << endl;
+    cout << "Size of double => " << sizeof(double) << " bytes" << endl;
+    return 0;
+}
