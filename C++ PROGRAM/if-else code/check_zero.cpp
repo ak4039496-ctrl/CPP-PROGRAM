e3a924b59514a8f85b780c8b3a591a090f2c0011
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-13 19:25:18 +0530

    check_zero

---
+++ C++ PROGRAM/if-else code/check_zero.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Check if a number is zero 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number:- ";
+    cin >> num;
+
+    if (num == 0) {
+        cout << "The number is Zero." << endl;
+    } else {
+        cout << "The number is not Zero." << endl;
+    }
+
+    return 0;
+}
