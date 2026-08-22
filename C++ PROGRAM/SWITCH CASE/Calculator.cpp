bc53ba6d8f0c7a2ffbbb522bbc1bc230252288a0
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-22 16:55:12 +0530

    Calculator

---
+++ C++ PROGRAM/SWITCH CASE/Calculator.cpp
@@ -0,0 +1,34 @@
+/*
+Author: Amit Gupta
+Date: 18-Aug-2026
+Program: Simple calculator using switch case
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    char op;
+    cout << "Enter two numbers:- ";
+    cin >> a >> b;
+    cout << "Enter operator (+,-,*,/):- ";
+    cin >> op;
+
+    switch(op) {
+        case '+': 
+        cout << "Sum => " << a+b; 
+        break;
+        case '-': 
+        cout << "Difference => " << a-b; 
+        break;
+        case '*': 
+        cout << "Product => " << a*b; 
+        break;
+        case '/': 
+        cout << "Quotient => " << a/b; break;
+        default: 
+        cout << "Invalid operator";
+    }
+    return 0;
+}
