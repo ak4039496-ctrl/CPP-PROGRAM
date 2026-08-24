727732d4e957fc4e38c71dc2b67c74b83744565f
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-24 21:52:56 +0530

    Grade_system

---
+++ C++ PROGRAM/SWITCH CASE/Grade_system.cpp
@@ -0,0 +1,24 @@
+/*
+Author: Amit Gupta
+Date: 19-Aug-2026
+Print grade 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cout << "Enter marks:- ";
+    cin >> marks;
+
+    switch(marks/10) {
+        case 10:
+        case 9: cout << "Grade A"; break;
+        case 8: cout << "Grade B"; break;
+        case 7: cout << "Grade C"; break;
+        case 6: cout << "Grade D"; break;
+        default: cout << "Grade F";
+    }
+    return 0;
+}
