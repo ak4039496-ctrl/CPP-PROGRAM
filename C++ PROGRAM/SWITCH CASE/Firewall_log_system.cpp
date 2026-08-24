d376e1fc19f6063078c697bf077c65f121bfc3e6
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-24 21:50:22 +0530

    Firewall_log_system

---
+++ C++ PROGRAM/SWITCH CASE/Firewall_log_system.cpp
@@ -0,0 +1,24 @@
+/*
+Author: Amit Gupta
+Date: 19-Aug-2026
+Firewall log system 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int port; // port number
+    cout << "Enter port number:- ";
+    cin >> port;
+
+    switch(port) {
+        case 80: cout << "Log: HTTP Traffic";  // web
+        break;  
+        case 443: cout << "Log: HTTPS Traffic"; break; // secure web
+        case 21: cout << "Log: FTP Blocked"; break;    // file transfer
+        case 22: cout << "Log: SSH Traffic"; break;    // secure shell
+        default: cout << "Unknown port!";              // wrong input
+    }
+    return 0;
+}
