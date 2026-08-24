5818eb5f49b1228c7127861fd7ba5d96ae5b36bc
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-24 21:51:15 +0530

    Food_delivery

---
+++ C++ PROGRAM/SWITCH CASE/Food_delivery.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Amit Gupta
+Date: 19-Aug-2026
+Food delivery system 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int food; // food code
+    cout << "Enter food (1=Pizza,2=Burger,3=Sandwich):- ";
+    cin >> food;
+
+    switch(food) {
+        case 1: cout << "Order placed: Pizza"; break;
+        case 2: cout << "Order placed: Burger"; break;
+        case 3: cout << "Order placed: Sandwich"; break;
+        default: cout << "Invalid food choice!";
+    }
+    return 0;
+}
