c433a0293aad9cc3546ead34c2485bd7c1fff2a5
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-08-29 17:33:40 +0530

    Vowel  -  check

---
+++ C++ PROGRAM/SWITCH CASE/Vowel_check.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Amit Gupta
+Date: 22-Aug-2026
+Check vowel or consonant
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter character:- ";
+    cin >> ch;
+
+    switch(ch) {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            cout << "Vowel"; break;
+        default: cout << "Consonant";
+    }
+    return 0;
+}
